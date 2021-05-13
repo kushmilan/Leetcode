c66625114b314c9b7c063784e61be2faed8f970d
Milan Singh Kushwah <33677132+kushmilan@users.noreply.github.com>
2021-05-13 15:10:26 +0530

    added new Solutions

--- Easy/Remove Nth Node From End of List.java
+++ Easy/Remove Nth Node From End of List.java
@@ -26,3 +26,33 @@ class Solution {
         return head.next;
     }
 }
+
+
+//------------------------------------------(REcursive)---------------------------
+
+class Solution {
+    static int val;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null) return head;
+        
+        val = n;
+        
+        return removeFromLast(head);
+    }
+    
+    public static ListNode removeFromLast(ListNode head){                
+        
+        if(head != null && head.next != null){
+            removeFromLast(head.next);
+
+            val--;
+
+            if(val == 0) head.next = head.next.next;
+        }
+        
+        if(val == 1) return head.next;
+        
+        return head;
+        
+    }
+}
