ab4f8880b057af8d0885e675179620961fa2a1de
Milan Singh Kushwah <33677132+kushmilan@users.noreply.github.com>
2021-05-13 07:55:22 +0530

    New solution added

---
+++ Medium/Longest Substring Without Repeating Characters.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int i = 0, j = 0, max = 0;
+        
+        Set<Character> set = new HashSet<>();
+        
+        while(j < s.length()){                        
+            if(!set.contains(s.charAt(j))){                
+                set.add(s.charAt(j++));
+                max = Math.max(set.size(), max);
+            }else{                                
+                set.remove(s.charAt(i++));
+            }
+        }
+        
+        return max;
+    }
+}
