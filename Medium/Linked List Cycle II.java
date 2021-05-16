55bbb67320f36f784e7c8c584affff6f9cc87291
Milan Singh Kushwah <33677132+kushmilan@users.noreply.github.com>
2021-05-16 23:17:17 +0530

    New Solution added

--- Medium/Linked List Cycle II.java
+++ Medium/Linked List Cycle II.java
@@ -26,3 +26,26 @@ public class Solution {
 
 
 //--------------------------------------------------------------------------------
+
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null) return null;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                while(head != fast){
+                    head = head.next;
+                    fast = fast.next;
+                }
+                return head;
+            }                        
+        }        
+        return null;
+    }
+}
